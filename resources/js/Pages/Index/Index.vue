79f4018bee74fb3fe75af4a25732e52d48e82e28
cabavas <cabavas@gmail.com>
2025-01-07 11:51:38 -0300

    First commit

---
+++ resources/js/Pages/Index/Index.vue
@@ -0,0 +1,12 @@
+<template>
+    <MainLayout>
+        <div>Index</div>
+    </MainLayout>
+</template>
+
+<script setup>
+import MainLayout from "../../Layouts/MainLayout.vue";
+defineProps({
+        message: String
+    })
+</script>
---
+++ resources/js/Pages/Index/Show.vue
@@ -0,0 +1,9 @@
+<template>
+    <MainLayout>
+        <div>Show</div>
+    </MainLayout>
+</template>
+
+<script setup>
+import MainLayout from "../../Layouts/MainLayout.vue";
+</script>
