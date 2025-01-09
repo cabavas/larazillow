90b4136d83797e5e33509b44cff765f0afadbdbc
cabavas <cabavas@gmail.com>
2025-01-09 00:05:13 -0300

    Aula 69

--- resources/js/Composables/useMonthlyPayment.js
+++ resources/js/Composables/useMonthlyPayment.js
@@ -5,10 +5,14 @@ export const useMonthlyPayment = (total, interestRate, duration) => {
         const principle = isRef(total) ? total.value : total
         const monthlyInterest = (isRef(interestRate) ? interestRate.value : interestRate) / 100 / 12
         const numberOfPayments = (isRef(duration) ? duration.value : duration) * 12
-        
+
         return principle * monthlyInterest
             * (Math.pow(1 + monthlyInterest, numberOfPayments))
             / (Math.pow(1 + monthlyInterest, numberOfPayments) - 1)
     })
-    return monthlyPayment;
+    const totalPaid = computed(() => {
+        return (isRef(duration) ? duration.value : duration) * 12 * monthlyPayment.value
+    })
+    const totalInterest = computed(() => totalPaid.value - (isRef(total) ? total.value : total))
+    return { monthlyPayment, totalPaid, totalInterest }
 }
