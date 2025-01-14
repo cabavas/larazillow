d5eeed81ad957556abe117779aa60e6d463b8bc3
cabavas <cabavas@gmail.com>
2025-01-14 12:19:18 -0300

    Aula 107

---
+++ app/Http/Controllers/RealtorListingController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class RealtorListingController extends Controller
+{
+    public function index() {
+        // dd(Auth::user()->listings()->get()->first()->price);
+        return inertia('Realtor/Index', ['listings' => Auth::user()->listings]);
+    }
+}
