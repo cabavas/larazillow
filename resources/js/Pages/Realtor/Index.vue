4f9ce06d2d3b2cd368b31104a9bc106078c1708c
cabavas <cabavas@gmail.com>
2025-01-15 08:38:31 -0300

    Aula 116

--- resources/js/Pages/Realtor/Index.vue
+++ resources/js/Pages/Realtor/Index.vue
@@ -1,7 +1,7 @@
 <template>
     <h1 class="text-3xl mb-4">Your Listings</h1>
     <section class="mb-8">
-        <RealtorFilters />
+        <RealtorFilters :filters="filters" />
     </section>
     <section class="grid grid-cols-1 lg:grid-cols-2 gap-2">
         <Box v-for="listing in listings" :key="listing.id">
@@ -35,5 +35,8 @@ import ListingAddress from '../../Components/ListingAddress.vue';
 import { Link } from '@inertiajs/vue3';
 import RealtorFilters from './Index/Components/RealtorFilters.vue';
 
-defineProps({ listings: Array })
+defineProps({
+    listings: Array,
+    filters: Object,
+})
 </script>
